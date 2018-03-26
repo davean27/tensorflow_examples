518c8f4eb5c1648af59a8fda6e4d421095991fdf
hackin1101 <aninseok@gmail.com>
2018-03-26 13:22:06 +0900

    Change test case

--- Basics/linear_regression.py
+++ Basics/linear_regression.py
@@ -23,11 +23,9 @@ sess.run(tf.global_variables_initializer())
 
 for step in range(4001):
     cost_val, W_val, b_val, _ = sess.run([cost, W, b, train],
-                                         feed_dict={x: [1.0, 2.0, 3.0],
-                                                    y: [1.0, 2.0, 3.0]})
+                                         feed_dict={x: [1.0, 2.0, 3.0, 4.0],
+                                                    y: [1.1, 2.1, 3.1, 4.1]})
     if step % 50 == 0:
         print(step, cost_val, W_val, b_val)
 
 print(sess.run(h, feed_dict={x: [10, 20]}))
-
-# test our model
