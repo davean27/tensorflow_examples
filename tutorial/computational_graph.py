abc644f154be4a53c068b36319f5f81f980a014b
hackin1101 <aninseok@gmail.com>
2018-03-26 17:02:27 +0900

    create 'tutorial' directory and move some files into the directory

---
+++ tutorial/computational_graph.py
@@ -0,0 +1,17 @@
+import tensorflow as tf
+
+'''
+build graph
+'''
+const_node1 = tf.constant(3.0, tf.float32)
+const_node2 = tf.constant(4.0, tf.float32)
+com_node = tf.add(const_node1, const_node2) # or const_node1 + const_node2
+
+
+'''
+run graph
+'''
+sess = tf.Session()
+
+print("sess.run(node1, node2): ", sess.run([const_node1, const_node2]))
+print("sess.run(node3): ", sess.run(com_node))
--- tutorial/hello_world.py
+++ tutorial/hello_world.py
@@ -1,10 +1,5 @@
 import tensorflow as tf
 
-# Constant Node
 hello = tf.constant("Hello, TensorFlow!")
-
-# New Session
 sess = tf.Session()
-
-# Run
 print(sess.run(hello))
