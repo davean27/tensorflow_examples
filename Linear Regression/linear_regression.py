f7863856706e9966a6a3a341bde1d2cba4113d7d
ezkun <aninseok@gmail.com>
2018-03-27 12:32:49 +0900

    Change directories

--- Linear Regression/linear_regression.py
+++ Linear Regression/linear_regression.py
@@ -1,5 +1,8 @@
 import tensorflow as tf
 
+'''
+Build TensorFlow graph
+'''
 # Input train data set
 x = tf.placeholder(tf.float32, shape=[None])
 y = tf.placeholder(tf.float32, shape=[None])
@@ -15,21 +18,37 @@ hypothesis = x * W + b
 difference = hypothesis - y
 cost = tf.reduce_mean(tf.square(difference))
 
-
+# get train node
 optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.1)
 train = optimizer.minimize(cost)
 
-# Learning
+'''
+Run Tensorflow graph
+'''
 sess = tf.Session()
 
-# Variable 사용시 반드시 initializer 사용
+# Must initialize global variable before using it
 sess.run(tf.global_variables_initializer())
 
-for step in range(4001):
-    cost_val, W_val, b_val, _ = sess.run([cost, W, b, train],
-                                         feed_dict={x: [1.0, 2.0, 3.0, 4.0],
-                                                    y: [1.1, 2.1, 3.1, 4.1]})
-    if step % 50 == 0:
-        print(step, cost_val, W_val, b_val)
 
-print(sess.run(h, feed_dict={x: [10, 20]}))
+'''
+Learning iteration
+'''
+test_data = {x:[1.0, 2.0, 3.0, 4.0], y:[1.1, 2.1, 3.1, 4.1]}
+
+for step in range(2001):
+    cost_val, weight, bias, _ = sess.run([cost, W, b, train], feed_dict=test_data)
+
+    # trace learning step
+    if step % 50 == 0:
+        print('step :', step, end=', ')
+        print('cost_val :', cost_val, end=', ')
+        print('weight', weight, end=', ')
+        print('bias', bias )
+
+
+'''
+test : prediction
+'''
+print('Prediction x:10', sess.run(hypothesis, feed_dict=sess.run(hypothesis, feed_dict={x: [10, 20]})))
+
